2815424fc6977bfc0d7757dbac10f316b6ebfadd
xzs <18217358013@163.com>
2017-09-25 22:58:16 +0800

    【add】 初始化项目，创建了一个打印工具类

---
+++ src/FourthEdition/ThirdChapters/Third_1.java
@@ -0,0 +1,17 @@
+package FourthEdition.ThirdChapters;
+import static FourthEdition.util.PrintUtil.*;
+
+import java.util.Date;
+
+/**
+ * @Description:TODO
+ * @author: xzs
+ * @time:2017年9月25日 下午10:53:00
+ */
+public class Third_1 {
+
+	public static void main(String[] args) {
+		print("你好");
+		print(new Date());
+	}
+}
---
+++ src/FourthEdition/util/PrintUtil.java
@@ -0,0 +1,22 @@
+package FourthEdition.util;
+
+/**
+ * 打印方法工具类
+ * @Description:TODO
+ * @author: xzs
+ * @time:2017年9月25日 下午10:45:15
+ */
+public class PrintUtil {
+	/**
+	 * 
+	 * @Description:TODO
+	 * @param obj
+	 * void
+	 * @exception:
+	 * @author: xzs
+	 * @time:2017年9月25日 下午10:49:35
+	 */
+	public static void print(Object obj) {
+		System.out.println(obj);
+	}
+}
